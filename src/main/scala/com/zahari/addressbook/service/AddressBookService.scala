3e737384e64b5bbfb9cb9fda056825e59e050046
Zahari Dichev <zaharidichev@gmail.com>
2017-03-31 00:05:32 +0300

    Added a trait to define the methods of the service layer

---
+++ src/main/scala/com/zahari/addressbook/service/AddressBookService.scala
@@ -0,0 +1,12 @@
+package com.zahari.addressbook.service
+
+import com.zahari.addressbook.domain.Person
+
+/**
+  * Created by zahari on 30/03/2017.
+  */
+trait AddressBookService {
+  def numberOfMales: Int
+  def oldestPerson: Person
+  def ageDifferenceInDays(n1: String, n2: String): Int
+}
